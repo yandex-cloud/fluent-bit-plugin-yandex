62711e5230c5139da43eddb0fd6d821502f1ebd4
Mariia Smirnova <mariia-smi@yandex-team.ru>
2022-01-31 14:19:31 +0300

    created func for getMetadata

--- config.go
+++ config.go
@@ -19,13 +19,49 @@ func getConfigKey(plugin unsafe.Pointer, key string) string {
 	return strings.TrimSpace(output.FLBPluginConfigKey(plugin, key))
 }
 
+func getMetadata(key string) (string, error) {
+	const (
+		keyMetadataUrlEnv = "YC_METADATA_URL"
+		urlSuffix         = "/computeMetadata/v1/"
+		requestTimeout    = 5 * time.Second
+	)
+
+	metadataEndpoint := os.Getenv(keyMetadataUrlEnv)
+	if len(metadataEndpoint) == 0 {
+		metadataEndpoint = "http://" + ycsdk.InstanceMetadataAddr
+	}
+	urlMetadata := metadataEndpoint + urlSuffix + key
+
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, urlMetadata, nil)
+	if err != nil {
+		return "", fmt.Errorf("could not make request to autodetect folder ID: %s", err.Error())
+	}
+	req.Header.Set("Metadata-Flavor", "Google")
+
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	defer cancel()
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		return "", fmt.Errorf("could not get instance metadata to autodetect folder ID: %s", err.Error())
+	}
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("request to autodetect folder ID returned status other than OK: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("response body returned by request to autodetect folder ID read failed: %s", err.Error())
+	}
+
+	return string(body), nil
+}
+
 func getDestination(plugin unsafe.Pointer) (*logging.Destination, error) {
 	const (
-		keyFolderID       = "folder_id"
-		keyGroupID        = "group_id"
-		keyMetadataUrlEnv = "YC_METADATA_URL"
-		urlSuffixFolderID = "/computeMetadata/v1/yandex/folder-id"
-		requestTimeout    = 5 * time.Second
+		keyFolderID         = "folder_id"
+		keyGroupID          = "group_id"
+		metadataKeyFolderID = "yandex/folder-id"
 	)
 
 	if groupID := getConfigKey(plugin, keyGroupID); len(groupID) > 0 {
@@ -36,34 +72,10 @@ func getDestination(plugin unsafe.Pointer) (*logging.Destination, error) {
 		return &logging.Destination{Destination: &logging.Destination_FolderId{FolderId: folderID}}, nil
 	}
 
-	metadataEndpoint := os.Getenv(keyMetadataUrlEnv)
-	if len(metadataEndpoint) == 0 {
-		metadataEndpoint = "http://" + ycsdk.InstanceMetadataAddr
-	}
-	urlFolderID := metadataEndpoint + urlSuffixFolderID
-
-	client := http.Client{}
-	req, err := http.NewRequest(http.MethodGet, urlFolderID, nil)
+	folderId, err := getMetadata(metadataKeyFolderID)
 	if err != nil {
-		return nil, fmt.Errorf("could not make request to autodetect folder ID: %s", err.Error())
+		return nil, err
 	}
-	req.Header.Set("Metadata-Flavor", "Google")
-
-	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
-	defer cancel()
-	resp, err := client.Do(req.WithContext(ctx))
-	if err != nil {
-		return nil, fmt.Errorf("could not get instance metadata to autodetect folder ID: %s", err.Error())
-	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("request to autodetect folder ID returned status other than OK: %s", resp.Status)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("response body returned by request to autodetect folder ID read failed: %s", err.Error())
-	}
-	folderId := string(body)
 
 	return &logging.Destination{Destination: &logging.Destination_FolderId{FolderId: folderId}}, nil
 }
