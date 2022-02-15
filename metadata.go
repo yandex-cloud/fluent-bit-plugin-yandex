802a0006aa834c982eb38ea9e01fc268c720f552
Mariia Smirnova <mariia-smi@yandex-team.ru>
2022-02-15 17:53:14 +0300

    implemented getting all metadata into struct and getting value from it

--- metadata.go
+++ metadata.go
@@ -6,7 +6,11 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
+
+	"google.golang.org/protobuf/types/known/structpb"
 
 	ycsdk "github.com/yandex-cloud/go-sdk"
 )
@@ -51,4 +55,63 @@ func getMetadataValue(key string) (string, error) {
 	}
 
 	return string(body), nil
+}
+
+func getAllMetadata() (*structpb.Struct, error) {
+	const (
+		queryParam     = "?recursive=true"
+		requestTimeout = 5 * time.Second
+	)
+
+	urlMetadata := getMetadataUrl() + queryParam
+
+	client := http.Client{}
+	req, err := http.NewRequest(http.MethodGet, urlMetadata, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not make request to get all metadata: %s", err.Error())
+	}
+	req.Header.Set("Metadata-Flavor", "Google")
+
+	ctx, cancel := context.WithTimeout(context.Background(), requestTimeout)
+	defer cancel()
+	resp, err := client.Do(req.WithContext(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("could not get all metadata: %s", err.Error())
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("request to get all metadata returned status other than OK: %s", resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("response body returned by request to get all metadata read failed: %s", err.Error())
+	}
+
+	metadataStruct := new(structpb.Struct)
+	err = metadataStruct.UnmarshalJSON(body)
+	if err != nil {
+		return nil, fmt.Errorf("could not unmarshal response body returned by request to get all metadata: %s", err.Error())
+	}
+
+	return metadataStruct, nil
+}
+
+func getCachedMetadataValue(metadata *structpb.Struct, key string) (string, error) {
+	path := strings.Split(key, "/")
+	cur := structpb.NewStructValue(metadata)
+	for _, p := range path {
+		switch cur.GetKind().(type) {
+		case *structpb.Value_StructValue:
+			cur = cur.GetStructValue().GetFields()[p]
+		case *structpb.Value_ListValue:
+			index, err := strconv.Atoi(p)
+			if err != nil {
+				return "", fmt.Errorf("incorrect metadata key: %s", key)
+			}
+			cur = cur.GetListValue().GetValues()[index]
+		default:
+			return "", fmt.Errorf("incorrect metadata key: %s", key)
+		}
+	}
+	return cur.String(), nil
 }
