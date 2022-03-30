094ff716dfbca0ce363d2cd437f970da7207593d
Mariia Smirnova <mariia-smi@yandex-team.ru>
2022-03-30 17:16:45 +0300

    more tests for plugin init

--- pluginImpl_test.go
+++ pluginImpl_test.go
@@ -3,13 +3,24 @@ package main
 import (
 	"testing"
 
+	"github.com/fluent/fluent-bit-go/output"
+
 	"github.com/stretchr/testify/assert"
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/logging/v1"
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-func TestInit_WithGroupID_WithoutMetadataTemplate_Success(t *testing.T) {
-	configMap := map[string]string{
+var configMap map[string]string
+var getConfigValue = func(key string) string {
+	val, ok := configMap[key]
+	if ok {
+		return val
+	}
+	return ""
+}
+
+func TestInit_AllConfig_GroupID_Success(t *testing.T) {
+	configMap = map[string]string{
 		"level_key":       "level",
 		"message_key":     "message",
 		"message_tag_key": "message_tag",
@@ -19,13 +30,6 @@ func TestInit_WithGroupID_WithoutMetadataTemplate_Success(t *testing.T) {
 		"default_level":   "INFO",
 		"default_payload": "{}",
 		"authorization":   "instance-service-account",
-	}
-	getConfigValue := func(key string) string {
-		val, ok := configMap[key]
-		if ok {
-			return val
-		}
-		return ""
 	}
 	metadataProvider := TestMetadataProvider{}
 
@@ -41,4 +45,46 @@ func TestInit_WithGroupID_WithoutMetadataTemplate_Success(t *testing.T) {
 	assert.Equal(t, "message_tag", plugin.keys.messageTag, "incorrect message tag key")
 	assert.Equal(t, &template{format: "resource_type", keys: [][]string{}}, plugin.keys.resourceType, "incorrect resource type")
 	assert.Equal(t, &template{format: "resource_id", keys: [][]string{}}, plugin.keys.resourceID, "incorrect resource id")
+}
+
+func TestInit_FolderID_Success(t *testing.T) {
+	configMap = map[string]string{
+		"folder_id":     "folder_id",
+		"authorization": "instance-service-account",
+	}
+	metadataProvider := TestMetadataProvider{}
+
+	plugin := new(pluginImpl)
+	_, err := plugin.init(getConfigValue, metadataProvider)
+
+	assert.Nil(t, err)
+	assert.Equal(t, &logging.Destination{Destination: &logging.Destination_FolderId{FolderId: "folder_id"}}, plugin.destination, "incorrect destination")
+}
+
+func TestInit_FolderIDAutodetection_Success(t *testing.T) {
+	configMap = map[string]string{
+		"authorization": "instance-service-account",
+	}
+	metadataProvider := TestMetadataProvider{
+		"yandex/folder-id": "folder-id",
+	}
+
+	plugin := new(pluginImpl)
+	_, err := plugin.init(getConfigValue, metadataProvider)
+
+	assert.Nil(t, err)
+	assert.Equal(t, &logging.Destination{Destination: &logging.Destination_FolderId{FolderId: "folder-id"}}, plugin.destination, "incorrect destination")
+}
+
+func TestInit_FolderIDAutodetection_Fail(t *testing.T) {
+	configMap = map[string]string{
+		"authorization": "instance-service-account",
+	}
+	metadataProvider := TestMetadataProvider{}
+
+	plugin := new(pluginImpl)
+	code, err := plugin.init(getConfigValue, metadataProvider)
+
+	assert.Equal(t, output.FLB_ERROR, code)
+	assert.NotNil(t, err)
 }
