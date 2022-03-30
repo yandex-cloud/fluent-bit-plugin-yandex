eeadf4b73089b8823e0ea569ac03c80e2b362b4a
Mariia Smirnova <mariia-smi@yandex-team.ru>
2022-03-30 17:36:35 +0300

    tests for getRecordValue

---
+++ util_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetRecordValue_Success(t *testing.T) {
+	record := map[interface{}]interface{}{
+		"a": map[interface{}]interface{}{
+			"b": "value",
+		},
+	}
+	path := []string{"a", "b"}
+
+	val, err := getRecordValue(record, path)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "value", val, "incorrect value")
+}
+
+func TestGetRecordValue_WithArray_Success(t *testing.T) {
+	record := map[interface{}]interface{}{
+		"a": []interface{}{"value"},
+	}
+	path := []string{"a", "0"}
+
+	val, err := getRecordValue(record, path)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "value", val, "incorrect value")
+}
+
+func TestGetRecordValue_JSONValue_Success(t *testing.T) {
+	record := map[interface{}]interface{}{
+		"a": map[interface{}]interface{}{
+			"b": map[interface{}]interface{}{
+				"first":  "1st",
+				"second": "2nd",
+			},
+		},
+	}
+	path := []string{"a", "b"}
+
+	val, err := getRecordValue(record, path)
+
+	assert.Nil(t, err)
+	assert.Equal(t, "{\"first\":\"1st\",\"second\":\"2nd\"}", val, "incorrect value")
+}
+
+func TestGetRecordValue_Fail(t *testing.T) {
+	record := map[interface{}]interface{}{
+		"a": 123,
+	}
+	path := []string{"a", "b"}
+
+	_, err := getRecordValue(record, path)
+
+	assert.NotNil(t, err)
+}
+
+func TestGetRecordValue_WithArray_Fail(t *testing.T) {
+	record := map[interface{}]interface{}{
+		"a": []interface{}{"value"},
+	}
+	path := []string{"a", "b"}
+
+	_, err := getRecordValue(record, path)
+
+	assert.NotNil(t, err)
+}
+
+func TestGetRecordValue_WithArray_OutOfBound_Fail(t *testing.T) {
+	record := map[interface{}]interface{}{
+		"a": []interface{}{"value"},
+	}
+	path := []string{"a", "1"}
+
+	_, err := getRecordValue(record, path)
+
+	assert.NotNil(t, err)
+}
