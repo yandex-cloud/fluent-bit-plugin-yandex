3b615c45b9f758ac2dc47c42790168b53920f229
Mariia Smirnova <mariia-smi@yandex-team.ru>
2022-04-08 18:29:45 +0300

    moved splitting into batches into WriteAll

--- plugin/write.go
+++ plugin/write.go
@@ -10,37 +10,45 @@ import (
 )
 
 func (p *Plugin) WriteAll(resourceToEntries map[Resource][]*logging.IncomingLogEntry) (results chan error, resCount int) {
-	resCount = len(resourceToEntries)
+	const batchMaxLen = 100
+	resCount = 0
+	for _, entries := range resourceToEntries {
+		resCount += (len(entries) + batchMaxLen - 1) / batchMaxLen
+	}
 	results = make(chan error, resCount)
 
 	for resource, entries := range resourceToEntries {
 		resource := resource.LogEntryResource()
 		entries := entries
 
-		go func(res chan error) {
-			err := p.Write(context.Background(), entries, resource)
-			res <- err
-		}(results)
+		for len(entries) > 0 {
+			var batch []*logging.IncomingLogEntry
+			if len(entries) > batchMaxLen {
+				batch, entries = entries[:batchMaxLen], entries[batchMaxLen:]
+			} else {
+				batch, entries = entries, nil
+			}
+
+			go func(res chan error) {
+				err := p.write(context.Background(), batch, resource)
+				res <- err
+			}(results)
+		}
 	}
 
 	return results, resCount
 }
 
-func (p *Plugin) Write(ctx context.Context, entries []*logging.IncomingLogEntry, resource *logging.LogEntryResource) error {
+func (p *Plugin) write(ctx context.Context, entries []*logging.IncomingLogEntry, resource *logging.LogEntryResource) error {
 	p.mu.RLock()
 	defer p.mu.RUnlock()
-	const batchMaxLen = 100
-	for len(entries) > 0 {
-		var batch []*logging.IncomingLogEntry
-		if len(entries) > batchMaxLen {
-			batch, entries = entries[:batchMaxLen], entries[batchMaxLen:]
-		} else {
-			batch, entries = entries, nil
-		}
+
+	toSend := entries
+	for len(toSend) > 0 {
 		failed, err := p.client.Write(ctx, &logging.WriteRequest{
 			Destination: p.destination,
 			Resource:    resource,
-			Entries:     batch,
+			Entries:     toSend,
 			Defaults:    p.defaults,
 		})
 		if err != nil {
@@ -56,20 +64,19 @@ func (p *Plugin) Write(ctx context.Context, entries []*logging.IncomingLogEntry,
 				codes.Unknown,
 				codes.Canceled,
 				codes.DeadlineExceeded:
-				toRetry = append(toRetry, batch[idx])
+				toRetry = append(toRetry, toSend[idx])
 			default:
 				p.printMu.Lock()
 				// bad message, just print
 				fmt.Printf(
 					"yc-logging: bad message %q: %q\n",
-					truncate(batch[idx].GetMessage(), 512),
+					truncate(toSend[idx].GetMessage(), 512),
 					failure.String(),
 				)
 				p.printMu.Unlock()
 			}
 		}
-		// add back to retry later
-		entries = append(toRetry, entries...)
+		toSend = toRetry
 	}
 	return nil
 }
